a83141a3ce4f2e6c2b14cac7a002cd2d30bddb40
masha-manyanina <mari.manuanina@gmail.com>
2025-03-28 04:39:52 +0300

    1-5

--- javascripts/animation.js
+++ javascripts/animation.js
@@ -1,1 +1,60 @@
-console.log('хочу 10 за предпросмотр!')
+document.addEventListener('DOMContentLoaded', () => {
+    unloading()
+    painting()
+    otherwise()
+    grow()
+
+})
+ 
+// --------------------------------------- разгрузить мысли -------------------------------
+function unloading() {
+
+     const images = document.querySelectorAll('.el')
+     
+     images.forEach((image) => {
+        image.addEventListener('click', () => {
+            image.style.opacity = '0'
+        })
+     })
+}
+
+
+// --------------------------------------- увидеть краски жизни -------------------------------
+function painting() {
+    const images = document.querySelectorAll('.pink')
+
+    images.forEach((image) => {
+        image.addEventListener('click', () => {
+            image.style.transform = 'scaleX(-1)'
+        })
+    })
+}
+
+
+
+// --------------------------------------- посмотреть на мир иначе -------------------------------
+function otherwise() {
+    const images = document.querySelectorAll('.otherwise')
+
+    images.forEach((image) => {
+        image.addEventListener('click', () => {
+            if (image.style.transform = 'rotate(0deg)') {
+                    image.style.transform = 'rotate(180deg)'
+            } else {
+                image.style.transform = 'rotate(0deg)'
+            }
+            }) 
+    })
+}
+
+// --------------------------------------- взрастить уверенность в себе -------------------------------
+function grow() {
+    const images = document.querySelectorAll('.grow')
+
+    images.forEach((image) => {
+        image.addEventListener('click', () => {
+            image.style.width = '23vw'
+            image.style.height = '23vw'
+        })
+    })
+}
